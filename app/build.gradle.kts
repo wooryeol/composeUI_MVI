455560edee0fe0ff15c295095d289672a7159c29
wooryeol <>
2026-04-16 09:12:42 +0900

    @date: 2026-04-16 @auther: 김우렬 @version: 26041601:0.1 @comment: README 파일 위치 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.example.compose"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26041502
+        versionCode = 26041601
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
